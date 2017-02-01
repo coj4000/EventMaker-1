770feb063c4ccc658c353c8cf5ba44d60078e856
Rolf Petersen <rollep7@msn.com>
2017-02-01 11:00:48 +0100

    PersistencyService class

---
+++ EventMaker/Persistency/PersistencyService.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using EventMaker.Model;
+
+namespace EventMaker.Persistency
+{
+    class PersistencyService
+    {
+        public static async void SaveEventAsJsonAsync(ObservableCollection<Event> events)
+        {
+            
+        }
+
+        public static async Task<List<Event>> LoadEventsFromJsonAsync()
+        {
+            
+        }
+
+        public static async void SerializeEventsFileAsync(string eventsString, string fileName)
+        {
+            
+        }
+
+        public static async Task<string> DeSerializeEventFileAsync(string fileName)
+        {
+            
+        }
+    }
+}
